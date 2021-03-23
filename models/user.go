fb85421bb8cb4ce35bcaf4881687bd526f6c30ae
lnelectronic <63729773+lnelectronic@users.noreply.github.com>
2021-03-24 05:04:04 +0700

    - Fixed user Struct Insertuser

--- models/user.go
+++ models/user.go
@@ -1,14 +1,16 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // user insert
 type User struct {
 	ID          primitive.ObjectID `bson:"_id" json:"id"`
-	Username    string             `bson:"username" json:"username"`
+	Username    string             `bson:"username" json:"username" validate:"required"`
 	Passwd      string             `bson:"passwd" json:"passwd"`
-	Email       string             `bson:"email" json:"email"`
-	Phone       int32              `bson:"phone" json:"phone"`
+	Email       string             `bson:"email" json:"email" validate:"required,email"`
+	Phone       int64              `bson:"phone" json:"phone"`
 	AccessLevel []string           `bson:"accessLevel" json:"accessLevel"`
 }
 
